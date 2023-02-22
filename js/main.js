0db83a24b5aa607aa8fa4b9ec629f01ceecd4343
erimustafa <erismustaffa@gmail.com>
2023-02-22 21:51:24 +0100

    slideshow and alert
    added slideshow and onclick alert

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
-onclick="document.getElementById('demo').innerHTML = Date()">
+function myAlert() {
+	alert("Sorry this page can not open!")
+}
+
+var slideIndex=1;
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('slide');
+
+    for ( i=0; i < slides.length; i++){
+    	slides[i].style.display = "none";
+    }
+
+    if( n > slides.length){
+    	slideIndex=1;
+    }
+    if(n < 1){
+    	slideIndex = slides.length;
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
+
+showSlides(1);
+
+function pulseSlide(n){
+	slideIndex +=n;
+	showSlides(slideIndex);
+}
+
+setInterval( pulseSlide,2000,1);
+
+
 
